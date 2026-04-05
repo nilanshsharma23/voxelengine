8cc7821c5cb698f8aafee5e202cb1220cb595a9d
Nilansh Sharma <80972022+nilanshsharma23@users.noreply.github.com>
2026-04-05 21:53:10 +0530

    gonma try something new

--- src/main/java/com/pyscrap/terrain/World.java
+++ src/main/java/com/pyscrap/terrain/World.java
@@ -2,32 +2,26 @@ package com.pyscrap.terrain;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
-
 import com.pyscrap.renderEngine.MasterRenderer;
 import com.pyscrap.textures.ModelTexture;
 
 public class World {
-    public static int CHUNK_LENGTH = 16;
-    public static int CHUNK_HEIGHT = 16;
-    public static int NUMBER_OF_CHUNKS_X = 16;
-    public static int NUMBER_OF_CHUNKS_Z = 16;
+    public static int CHUNK_LENGTH = 32;
+    public static int CHUNK_HEIGHT = 32;
+    public static int NUMBER_OF_CHUNKS_X = 8;
+    public static int NUMBER_OF_CHUNKS_Z = 8;
+    public static int FREQUENCY = 1 / 32;
 
     List<Chunk> chunks = new ArrayList<>();
 
     public World(List<ModelTexture> textures, MasterRenderer renderer) {
-        Random random = new Random();
-        NoiseGenerator noiseGenerator = new NoiseGenerator();
-        noiseGenerator.setSeed(random.nextInt());
-
         byte[][][] blocks = new byte[NUMBER_OF_CHUNKS_X * CHUNK_LENGTH][CHUNK_HEIGHT][NUMBER_OF_CHUNKS_Z
                 * CHUNK_LENGTH];
 
         for (int x = 0; x < CHUNK_LENGTH * NUMBER_OF_CHUNKS_X; x++) {
             for (int z = 0; z < CHUNK_LENGTH * NUMBER_OF_CHUNKS_Z; z++) {
                 for (int y = 0; y < CHUNK_HEIGHT; y++) {
-                    double noise = noiseGenerator.noise(x, y);
-                    if (y < noise) {
+                    if (y < 3) {
                         blocks[x][y][z] = BlockType.DIRT;
                     }
                 }
